2f8aaed22b61b382acb3e81bc1871c2a4f51af99
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-05-17 19:59:20 -0500

    Added Bands in Town API Query functionality&moment

--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
+var moment = require('moment');
+var date;
+var newMoment;
 // var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var input = process.argv[3]
-
-
-
 
 
 if (command == "movie-this" && input) {
@@ -37,5 +37,28 @@ Plot: ${response.data.Plot}
 Actors: ${response.data.Actors}`);
         }
     );
+}
+
+if (command == "concert-this" && input) {
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log(response.data[0]);
+            for (var i = 0; i < response.data.length; i++) {
+                date = response.data[i].datetime;
+                newMoment = moment(date);
+                if(!response.data[i].venue.region){
+                    console.log(`
+Venue: ${response.data[i].venue.name}
+Location: ${response.data[i].venue.city}, ${response.data[i].venue.country}
+Date: ${newMoment.format("MMM Do YYYY")}`);
+                } else {
+                    console.log(`
+Venue: ${response.data[i].venue.name}
+Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}
+Date: ${newMoment.format("MMM Do YYYY")}`);
+                }
+            }
+        }
+    )
 }
 

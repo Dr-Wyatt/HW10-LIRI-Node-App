21163f84ace8365bdc59c986fe3283a736060b18
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-05-18 16:00:33 -0500

    do what it says is functional

--- liri.js
+++ liri.js
@@ -3,75 +3,121 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require('moment');
 var Spotify = require('node-spotify-api');
+var fs = require('fs');
 var date;
 var newMoment;
 var spotify = new Spotify(keys.spotify);
-// var spotify = new Spotify({
-//     id: '8f59f1d3b4b44932b67b4040aa8a2955',
-//     secret: 'ca17157b5d1440bb96aa01fcb27f2552'
-//   });
-console.log(spotify);
 var command = process.argv[2];
 var input = process.argv[3]
 
-spotify.search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-
-if (command == "movie-this" && input) {
-    axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy").then(
-        function (response) {
-            console.log(`
-Title: ${response.data.Title}
-Year: ${response.data.Year}
-IMDB Rating: ${response.data.imdbRating}
-Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
-Country of Release: ${response.data.Country}
-Language of Movie: ${response.data.Language}
-Plot: ${response.data.Plot}
-Actors: ${response.data.Actors}`);
-        }
-    );
-} else if (command == "movie-this" && !input) {
-    axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
-        function (response) {
-            console.log(`
-Title: ${response.data.Title}
-Year: ${response.data.Year}
-IMDB Rating: ${response.data.imdbRating}
-Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
-Country of Release: ${response.data.Country}
-Language of Movie: ${response.data.Language}
-Plot: ${response.data.Plot}
-Actors: ${response.data.Actors}`);
-        }
-    );
+function runChecker(command, input) {
+    spotifyFunc(command, input);
+    movieFunc(command, input);
+    bandFunc(command, input);
 }
 
-if (command == "concert-this" && input) {
-    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
-        function (response) {
-            console.log(response.data[0]);
-            for (var i = 0; i < response.data.length; i++) {
-                date = response.data[i].datetime;
-                newMoment = moment(date);
-                if(!response.data[i].venue.region){
-                    console.log(`
-Venue: ${response.data[i].venue.name}
-Location: ${response.data[i].venue.city}, ${response.data[i].venue.country}
-Date: ${newMoment.format("MMM Do YYYY")}`);
-                } else {
-                    console.log(`
-Venue: ${response.data[i].venue.name}
-Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}
-Date: ${newMoment.format("MMM Do YYYY")}`);
+function spotifyFunc(command, input) {
+    if (command == "spotify-this-song" && input) {
+        spotify.search({ type: 'track', query: input, limit: 1 })
+            .then(function (response) {
+                console.log(`
+    Artist Name: ${response.tracks.items[0].album.artists[0].name}
+    Album Name: ${response.tracks.items[0].album.name}
+    Song Name: ${response.tracks.items[0].name}
+    Preview: ${response.tracks.items[0].preview_url}`)
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    } else if (command == "spotify-this-song" && !input) {
+        spotify.search({ type: 'track', query: 'The Sign' })
+            .then(function (response) {
+                console.log(`
+    Artist Name: ${response.tracks.items[7].album.artists[0].name}
+    Album Name: ${response.tracks.items[7].album.name}
+    Song Name: ${response.tracks.items[7].name}
+    Preview: ${response.tracks.items[7].preview_url}`)
+            }).catch(function (err) {
+                console.log(err);
+            });
+    }
+}
+
+
+function movieFunc(command, input) {
+    if (command == "movie-this" && input) {
+        axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+                console.log(`
+    Title: ${response.data.Title}
+    Year: ${response.data.Year}
+    IMDB Rating: ${response.data.imdbRating}
+    Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
+    Country of Release: ${response.data.Country}
+    Language of Movie: ${response.data.Language}
+    Plot: ${response.data.Plot}
+    Actors: ${response.data.Actors}`);
+            }
+        );
+    } else if (command == "movie-this" && !input) {
+        axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+                console.log(`
+    Title: ${response.data.Title}
+    Year: ${response.data.Year}
+    IMDB Rating: ${response.data.imdbRating}
+    Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
+    Country of Release: ${response.data.Country}
+    Language of Movie: ${response.data.Language}
+    Plot: ${response.data.Plot}
+    Actors: ${response.data.Actors}`);
+            }
+        );
+    }
+}
+
+function bandFunc() {
+    console.log("command: ", command);
+    console.log("input: ", input)
+    if (command == "concert-this" && input) {
+        axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                console.log(response.data[0]);
+                for (var i = 0; i < response.data.length; i++) {
+                    date = response.data[i].datetime;
+                    newMoment = moment(date);
+                    if (!response.data[i].venue.region) {
+                        console.log(`
+    Venue: ${response.data[i].venue.name}
+    Location: ${response.data[i].venue.city}, ${response.data[i].venue.country}
+    Date: ${newMoment.format("MMM Do YYYY")}`);
+                    } else {
+                        console.log(`
+    Venue: ${response.data[i].venue.name}
+    Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}
+    Date: ${newMoment.format("MMM Do YYYY")}`);
+                    }
                 }
             }
-        }
-    )
+        )
+    }
 }
 
+if (command == "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+        var dataArr = data.split(",")
+        console.log(dataArr);
+        command = dataArr[0];
+        input = dataArr[1].replace(/"/g, "");;
+        console.log("Command: ", command);
+        console.log("Input: ", input);
+        runChecker(command, input);
+
+
+    });
+}
+runChecker(command, input);
